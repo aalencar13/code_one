734af555a17383dd38cdcc091ceab5b9dac633df
aalencar13 <aalencar13@hotmail.com>
2022-05-21 12:56:20 -0300

    alteração de indentação e lógica do while
    retirada de palavra reservada True no trecho de código while após inclusçao da palavra reservada 'in'. alterações das indentações incorretas nos códigos.

--- desafio_python.py
+++ desafio_python.py
@@ -6,13 +6,13 @@ print(f'Olá {nome.title()}, seja bem vindo ao jogo da forca!')
 input('Pressione a tecla enter para começar o jogo: ')
 os.system('cls')
 
-lista_de_palavras = ['banana', 'laranja', 'abacaxi','limão', 'uva','morango']
+lista_de_palavras = ['banana', 'laranja', 'abacaxi','limão', 'uva']
 palavra_selecionada = random.choice(lista_de_palavras).upper()
 tamanho_palavra = len(palavra_selecionada)
 palavra_codificada = ['_']*tamanho_palavra
 quantidade_de_erros = 0
 
-while '_' in palavra_codificada == True and quantidade_de_erros < 6:
+while '_' in palavra_codificada and quantidade_de_erros < 6:
     print(f'Sua palavra tem {tamanho_palavra} letras!')
     print(f'Erros: {quantidade_de_erros} de 6 tentativas')
     for letra in palavra_codificada:
@@ -24,16 +24,16 @@ while '_' in palavra_codificada == True and quantidade_de_erros < 6:
     for i in range(tamanho_palavra):
         if letra_escolhida == palavra_selecionada[i]:
             palavra_codificada[i] = letra_escolhida
-        acertou = True
+            acertou = True
     if acertou == True:
         print('parabéns, acertou!')
     else:
         print('A letra não existe na palavra!')
         quantidade_de_erros += 1
     
-    if quantidade_de_erros == 6:
-        print('Você perdeu, foi mal ai!')
-    else:
-        print('Parabens, você ganhou.')
+if quantidade_de_erros == 6:
+    print('Você perdeu, foi mal ai!')
+else:
+    print('Parabens, você ganhou.')
 
 print(f'A palavra era {palavra_selecionada}!')
